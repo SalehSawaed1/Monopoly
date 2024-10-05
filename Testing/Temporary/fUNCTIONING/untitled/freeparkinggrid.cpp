63749d2e973b26f28d214fd601364e2fb940bdd3
SalehSawaed1 <salehsawaed16@gmail.com>
2024-10-05 23:21:14 +0300

    Delete Testing/Temporary directory

--- Testing/Temporary/fUNCTIONING/untitled/freeparkinggrid.cpp
+++
@@ -1,4 +0,0 @@
-#include "freeparkinggrid.h"
-
-FreeParkingGrid::FreeParkingGrid(PointF topLeft, PointF topRight, PointF bottomLeft, PointF bottomRight, const std::string& name)
-    : SpecialGrid(topLeft, topRight, bottomLeft, bottomRight, name) { }
